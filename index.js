ef018d9f7545a1170e05c674f2ab9a85942dfc6e
Eduardo Natale Costa <edunatalec@gmail.com>
2021-11-02 19:43:00 -0300

    Commit: Tue Nov  2 19:43:00 -03 2021

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const CronJob = require('cron').CronJob;
 const exec = require('child_process').exec;
 
 new CronJob(process.env.CRON_TIME || '* * * * *', () => {
-  console.log("Commit");
+  console.log("Commit: " + new Date());
   exec('sh commit.sh');
 }, null, true, 'America/Los_Angeles').start();
